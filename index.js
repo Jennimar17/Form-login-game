1ad5ab1cdba5b6077bdbf2b3edf5889fa0825037
unknown <jennimar17@gmail.com>
2020-09-02 04:01:26 -0300

    agregue archivo js

---
+++ index.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form")
+const inputPassword = document.querySelector("#pwd")
+
+const checkPassword = document.querySelector("#check-pwd")
+const checkTerms = document.querySelector("#check-terms")
+
+inputPassword.onblur = (e) => {
+    if (inputPassword.value.length < 8) {
+      checkPassword.textContent = "The password must be at least 8 characters long."
+      inputPassword.classList.add("input-error")
+    }
+  }
+  
+/*con esto quiero que la checkeada de los terminos se obligatoria
+para mandar el fomulario ???????????????? NO ME FUNCIONA */
+
+inputTerms.onblur = (e) =>  {
+    if (inputTerms.value.length = false) {
+        checkTerms.textContent = "You must accept the terms."
+        inputTerms.classList.add("input-error")
+
+    }
+}
